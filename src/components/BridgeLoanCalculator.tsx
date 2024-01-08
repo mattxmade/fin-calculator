797421ec81d95d7665108661508d3d34b1aba080
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-08 19:23:50 +0000

    Add form elements: heading | form input sections

--- src/components/BridgeLoanCalculator.tsx
+++ src/components/BridgeLoanCalculator.tsx
@@ -3,5 +3,75 @@ type BlcProps = {
 } & JSX.IntrinsicElements["form"];
 
 export default function BridgeLoanCalculator({ children, ...props }: BlcProps) {
-  return <form {...props}></form>;
+  return (
+    <form {...props}>
+      <h1>Bridging Loan Calcualtor</h1>
+
+      <section>
+        <label htmlFor="blc-property-value">Property value</label>
+        <input name="blc-property-value" type="text" defaultValue="£" />
+      </section>
+
+      <section>
+        <label htmlFor="blc-loan-amount">Loan amount required</label>
+        <input name="blc-loan-amount" type="text" defaultValue="£" />
+      </section>
+
+      <section>
+        <section>
+          <label htmlFor="blc-monthly-interest-rate">
+            Montly interest rate
+          </label>
+          <input
+            name="blc-monthly-interest-rate"
+            type="text"
+            defaultValue="1%"
+          />
+        </section>
+
+        <input
+          name="blc-monthly-interest-rate"
+          defaultValue="1"
+          type="range"
+          step="0.1"
+          min="0"
+          max="2"
+        />
+      </section>
+
+      <section>
+        <section>
+          <label htmlFor="blc-loan-term-length">
+            Long term lengths (months)
+          </label>
+          <input name="blc-loan-term-length" type="text" />
+        </section>
+
+        <input
+          name="blc-loan-term-length"
+          defaultValue="1"
+          type="range"
+          step="0.1"
+          min="0"
+          max="2"
+        />
+      </section>
+
+      <section>
+        <section>
+          <label htmlFor="blc-product-fee">Product fee</label>
+          <input name="blc-product-fee" type="text" defaultValue="2%" />
+        </section>
+
+        <input
+          name="blc-product-fee"
+          defaultValue="1"
+          type="range"
+          step="1"
+          min="1"
+          max="3"
+        />
+      </section>
+    </form>
+  );
 }
