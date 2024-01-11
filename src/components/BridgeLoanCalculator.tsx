12ae2326b2c06420329ac815b1b384b2f827c226
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-11 23:02:25 +0000

    Refactor BridgeLoanCalculator: Improve handler function logic; map elements from array; improve types

--- src/components/BridgeLoanCalculator.tsx
+++ src/components/BridgeLoanCalculator.tsx
@@ -1,18 +1,53 @@
 import { useCallback, useState } from "react";
 import CalculatorResult from "./CalculatorResult";
 import formatPrice from "../utils/formatPrice";
+import generateLabelName from "../utils/generateLabelName";
 
 type BlcProps = {
+  currency?: "£" | "$";
+  productFee?: ProductFeeInput;
+  propertyValue?: PropertyValueInput;
+  loanTermLength?: LoanTermLengthInput;
+  loanAmountRequired?: LoanAmountRequiredInput;
+  monthlyInterestRate?: MonthlyInterestRateInput;
   children?: React.ReactNode;
 } & JSX.IntrinsicElements["form"];
 
+type RangeInput = { steps: number; min: number; max: number };
+
+type PropertyValueInput = {
+  label?: string;
+};
+
+type LoanAmountRequiredInput = {
+  label?: string;
+};
+
+type MonthlyInterestRateInput = {
+  label?: string;
+  defaultValue: number;
+  range: RangeInput;
+};
+
+type LoanTermLengthInput = {
+  label?: string;
+  defaultValue: number;
+  range: RangeInput;
+};
+
+type ProductFeeInput = {
+  label?: string;
+  defaultValue: number;
+  range: RangeInput;
+};
+
 type BlcFormData = {
-  [index: string]: string;
-  "blc-property-value": string;
-  "blc-loan-amount": string;
-  "blc-monthly-interest-rate": string;
-  "blc-loan-term-length": string;
-  "blc-product-fee": string;
+  [index: string]: number;
+  "Property value": number;
+  "Loan amount required": number;
+  "Monthly interest rate": number;
+  "Loan term length": number;
+  "Product fee": number;
 };
 
 export type BlcResult = {
@@ -24,170 +59,171 @@ export type BlcResult = {
   "Loan to value": string;
 };
 
-export default function BridgeLoanCalculator({ children, ...props }: BlcProps) {
-  const [formData, setFormData] = useState<BlcFormData>({
-    "blc-property-value": "£",
-    "blc-loan-amount": "£",
-    "blc-monthly-interest-rate": "1%",
-    "blc-loan-term-length": "6",
-    "blc-product-fee": "2%",
-  });
+type InputOption = {
+  name: string;
+  label: string;
+  defaultValue: string | number;
+  type: "currency" | "percentage" | "number";
+  range: { steps: number; min: number; max: number } | null;
+};
+
+export default function BridgeLoanCalculator(props: BlcProps) {
+  const productValue = {
+    name: "Property value",
+    type: "currency" as "currency",
+    range: null,
+    defaultValue: 0,
+    label: props.propertyValue?.label ?? "Property value",
+  };
+
+  const loanAmountRequired = {
+    name: "Loan amount required",
+    type: "currency" as "percentage",
+    range: null,
+    defaultValue: 0,
+    label: props.loanAmountRequired?.label ?? "Loan amount required",
+  };
+
+  const monthlyInterestRate = {
+    name: "Monthly interest rate",
+    type: "percentage" as "percentage",
+    label: props.monthlyInterestRate?.label ?? "Monthly interest rate",
+    defaultValue: props.monthlyInterestRate?.defaultValue ?? 1,
+    range: props.monthlyInterestRate?.range ?? {
+      steps: 0.1,
+      min: 0,
+      max: 2,
+    },
+  };
+
+  const loanTermLength = {
+    name: "Loan term length",
+    type: "number" as "number",
+    label: props.loanTermLength?.label ?? "Loan term length (months)",
+    defaultValue: props.loanTermLength?.defaultValue ?? 6,
+    range: props.loanTermLength?.range ?? {
+      steps: 1,
+      min: 6,
+      max: 18,
+    },
+  };
+
+  const productFee = {
+    name: "Product fee",
+    type: "percentage" as "percentage",
+    label: props.loanTermLength?.label ?? "Product fee",
+    defaultValue: props.loanTermLength?.defaultValue ?? 2,
+    range: props.loanTermLength?.range ?? {
+      steps: 1,
+      min: 1,
+      max: 3,
+    },
+  };
+
+  const blcOptions = [
+    productValue,
+    loanAmountRequired,
+    monthlyInterestRate,
+    loanTermLength,
+    productFee,
+  ];
 
   const [result, setResult] = useState<BlcResult | null>();
 
-  const handleCalcInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const [formData, setFormData] = useState<BlcFormData>({
+    "Property value": productValue.defaultValue,
+    "Loan amount required": loanAmountRequired.defaultValue,
+    "Monthly interest rate": monthlyInterestRate.defaultValue,
+    "Loan term length": loanTermLength.defaultValue,
+    "Product fee": productFee.defaultValue,
+  });
+
+  const handleCalcInput = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    inputData: InputOption
+  ) => {
     const input = e.target as HTMLInputElement;
+    // const containsNumber = /\d/; // [1]
 
     const key = Object.keys(formData).find(
-      (formInput) => formInput === input.name
+      (formInput) => formInput === inputData.name
     );
 
     if (!key) return;
 
-    let validInput = false;
-    let inputValue = input.value;
-
-    if (input.type === "range") {
-      const numberInput = input.valueAsNumber;
-
-      if (key === "blc-monthly-interest-rate") {
-        numberInput >= 0 && numberInput <= 2
-          ? (validInput = true)
-          : (validInput = false);
-
-        if (!validInput) return;
-        return setFormData((prevFormData) => ({
-          ...prevFormData,
-          [key]: inputValue + "%",
-        }));
-      }
-
-      if (key === "blc-product-fee") {
-        numberInput >= 1 && numberInput <= 3
-          ? (validInput = true)
-          : (validInput = false);
-
-        if (!validInput) return;
-        return setFormData((prevFormData) => ({
-          ...prevFormData,
-          [key]: inputValue + "%",
-        }));
-      }
-
-      if (!inputValue) return;
-      return setFormData((prevFormData) => ({
-        ...prevFormData,
-        [key]: inputValue,
-      }));
-    }
-
-    const isMoneyValue = formData[key].startsWith("£");
-    const isPercentage = formData[key].endsWith("%");
-
-    inputValue.includes(",") &&
-      (inputValue = inputValue
+    let valid = false;
+    const inputValue = Number(
+      input.value
+        .trim()
         .split("")
-        .filter((val) => val !== "," && val)
-        .join(""));
+        .filter((value) => !isNaN(+value) || (value === "." && value))
+        .join("")
+    );
 
-    if (isMoneyValue) {
-      // VALIDATE CURRENCY TEXT INPUT
-
-      if (inputValue.startsWith("£")) validInput = true;
-      if (isNaN(Number(inputValue.slice(1)))) validInput = false;
-    } else if (isPercentage) {
-      // VALIDATE PERCENTAGE TEXT INPUT
-
-      const percentage = Number(inputValue.slice(0, inputValue.length - 1));
-
-      if (key === "blc-monthly-interest-rate")
-        percentage >= 0 && percentage <= 2
-          ? (validInput = true)
-          : (validInput = false);
-
-      if (key === "blc-product-fee")
-        percentage >= 1 && percentage <= 3
-          ? (validInput = true)
-          : (validInput = false);
-    } else {
-      // VALIDATE MONTHS TEXT INPUT
-
-      const months = Number(inputValue);
-
-      if (key === "blc-loan-term-length")
-        (months >= 6 && months <= 18) || inputValue === ""
-          ? (validInput = true)
-          : (validInput = false);
+    if (input.type !== "range") valid = true;
+    if (input.type === "range" && inputData.range) {
+      valid =
+        inputValue >= inputData.range.min && inputValue <= inputData.range.max;
     }
 
-    if (!validInput) return;
-    setFormData((prevFormData) => ({ ...prevFormData, [key]: inputValue }));
+    if (!valid) return;
+
+    switch (inputData.type) {
+      case "currency":
+        setFormData({ ...formData, [key]: inputValue });
+        break;
+
+      case "number":
+        setFormData({ ...formData, [key]: inputValue });
+        break;
+
+      case "percentage":
+        setFormData({ ...formData, [key]: inputValue });
+        break;
+    }
   };
 
   const handleFormSubmit = (
     e: React.MouseEvent<HTMLButtonElement> | React.TouchEvent<HTMLButtonElement>
   ) => {
     e.preventDefault();
+    const productFee = formData["Product fee"];
+    const propertyValue = formData["Property value"];
+    const loanAmount = formData["Loan amount required"];
+    const loanTermLength = formData["Loan term length"];
+    const monthlyInterestRate = formData["Monthly interest rate"];
 
-    const hasNumber = /\d/; // https://stackoverflow.com/a/63546092
+    if (
+      !propertyValue ||
+      !loanAmount ||
+      !monthlyInterestRate ||
+      !loanTermLength ||
+      !productFee
+    )
+      return;
 
-    if (Object.values(formData).every((value) => hasNumber.test(value))) {
-      let propertyValue,
-        loanAmount,
-        monthlyInterestRate,
-        loanTermLength,
-        productFee;
+    const netLoanAmount = loanAmount;
+    const interestAmount =
+      (loanAmount / 100) * monthlyInterestRate * loanTermLength;
 
-      propertyValue = +formData["blc-property-value"].slice(1);
-      loanAmount = +formData["blc-loan-amount"].slice(1);
+    const amountBeforeFees = netLoanAmount + interestAmount;
 
-      monthlyInterestRate = +formData["blc-monthly-interest-rate"].slice(
-        0,
-        formData["blc-monthly-interest-rate"].length - 1
-      );
+    const productFees = (amountBeforeFees / 100) * productFee;
+    const grossLoanAmount = amountBeforeFees + productFees;
+    const loanToValue = ((grossLoanAmount / propertyValue) * 100).toFixed(2);
 
-      loanTermLength = +formData["blc-loan-term-length"];
-      productFee = +formData["blc-product-fee"].slice(0, -1);
+    const calcResults = {} as BlcResult;
 
-      const netLoanAmount = loanAmount;
-      const interestAmount =
-        (loanAmount / 100) * monthlyInterestRate * loanTermLength;
+    calcResults["Net loan amount"] = formatPrice(netLoanAmount, true);
+    calcResults["Interest amount"] = formatPrice(interestAmount, true);
+    calcResults["Product fees"] = formatPrice(productFees, true);
+    calcResults["Gross loan amount"] = formatPrice(grossLoanAmount, true);
+    calcResults["Loan to value"] = loanToValue + "%";
 
-      const amountBeforeFees = netLoanAmount + interestAmount;
-
-      const productFees = (amountBeforeFees / 100) * productFee;
-      const grossLoanAmount = amountBeforeFees + productFees;
-      const loanToValue = ((grossLoanAmount / propertyValue) * 100).toFixed(2);
-
-      const calcResults = {} as BlcResult;
-
-      calcResults["Net loan amount"] = "£" + netLoanAmount;
-      calcResults["Interest amount"] = "£" + interestAmount;
-      calcResults["Product fees"] = "£" + productFees;
-      calcResults["Gross loan amount"] = "£" + grossLoanAmount;
-      calcResults["Loan to value"] = loanToValue + "%";
-
-      setResult(calcResults);
-    }
+    setResult(calcResults);
   };
 
   const clearResult = useCallback(() => setResult(null), [result]);
-
-  const propertyPriceAsNumber = Number(
-    formData["blc-property-value"]
-      .split("")
-      .filter((value) => !isNaN(+value) && value)
-      .join("")
-  );
-  const propertyPrice = formatPrice(propertyPriceAsNumber, true);
-
-  const loanAmountAsNumber = Number(
-    formData["blc-loan-amount"]
-      .split("")
-      .filter((value) => !isNaN(+value) && value)
-      .join("")
-  );
-  const loanAmount = formatPrice(loanAmountAsNumber, true);
 
   return (
     <>
@@ -201,112 +237,55 @@ export default function BridgeLoanCalculator({ children, ...props }: BlcProps) {
         <form id="blc-form" {...props}>
           <h1>Bridging Loan Calcualtor</h1>
 
-          <section>
-            <label htmlFor="blc-property-value">Property value</label>
-            <input
-              name="blc-property-value"
-              type="text"
-              placeholder="£"
-              onChange={handleCalcInput}
-              value={propertyPrice}
-            />
-          </section>
+          {blcOptions.map((option) => {
+            const labelName = generateLabelName(option.label);
 
-          <section>
-            <label htmlFor="blc-loan-amount">Loan amount required</label>
-            <input
-              name="blc-loan-amount"
-              type="text"
-              placeholder="£"
-              onChange={handleCalcInput}
-              value={loanAmount}
-            />
-          </section>
+            return !option.range ? (
+              <section key={labelName}>
+                <label htmlFor={labelName}>{option.label}</label>
+                <input
+                  name={labelName}
+                  type="text"
+                  value={formatPrice(formData[option.name], true)}
+                  onChange={(e) => handleCalcInput(e, option)}
+                />
+              </section>
+            ) : (
+              <section key={labelName}>
+                <section>
+                  <label htmlFor={labelName}>{option.label}</label>
+                  <input
+                    name={labelName}
+                    type="text"
+                    value={formData[option.name]}
+                    onChange={(e) => handleCalcInput(e, option)}
+                  />
+                </section>
 
-          <section>
-            <section>
-              <label htmlFor="blc-monthly-interest-rate">
-                Montly interest rate
-              </label>
-              <input
-                name="blc-monthly-interest-rate"
-                type="text"
-                placeholder="1%"
-                onChange={handleCalcInput}
-                value={formData["blc-monthly-interest-rate"]}
-              />
-            </section>
-
-            <input
-              name="blc-monthly-interest-rate"
-              value={formData["blc-monthly-interest-rate"].slice(
-                0,
-                formData["blc-monthly-interest-rate"].length - 1
-              )}
-              onChange={handleCalcInput}
-              type="range"
-              step="0.1"
-              min="0"
-              max="2"
-            />
-          </section>
-
-          <section>
-            <section>
-              <label htmlFor="blc-loan-term-length">
-                Long term length (months)
-              </label>
-              <input
-                name="blc-loan-term-length"
-                type="text"
-                placeholder="6"
-                onChange={handleCalcInput}
-                value={formData["blc-loan-term-length"]}
-              />
-            </section>
-
-            <input
-              name="blc-loan-term-length"
-              value={formData["blc-loan-term-length"]}
-              onChange={handleCalcInput}
-              type="range"
-              step="1"
-              min="6"
-              max="18"
-            />
-          </section>
-
-          <section>
-            <section>
-              <label htmlFor="blc-product-fee">Product fee</label>
-              <input
-                name="blc-product-fee"
-                type="text"
-                placeholder="2%"
-                onChange={handleCalcInput}
-                value={formData["blc-product-fee"]}
-              />
-            </section>
-
-            <input
-              name="blc-product-fee"
-              value={formData["blc-product-fee"].slice(
-                0,
-                formData["blc-product-fee"].length - 1
-              )}
-              onChange={handleCalcInput}
-              type="range"
-              step="1"
-              min="1"
-              max="3"
-            />
-          </section>
+                <input
+                  name={labelName}
+                  value={formData[option.name]}
+                  type="range"
+                  step={option.range.steps}
+                  min={option.range.min}
+                  max={option.range.max}
+                  onChange={(e) => handleCalcInput(e, option)}
+                />
+              </section>
+            );
+          })}
 
           <button type="submit" form="blc-form" onClick={handleFormSubmit}>
             Submit
           </button>
         </form>
       )}
+      {props.children}
     </>
   );
 }
+
+/*
+ [1] INFO: regex: return true if string contains a number
+     LINK: https://stackoverflow.com/a/63546092
+*/
