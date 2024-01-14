438997fb6afb3dd4326de4b22c280ceef52b74e6
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-14 21:37:31 +0000

    Update Component: Declare types, compose JSX elements + handle props

--- src/components/inputs/RangeInputSection.tsx
+++ src/components/inputs/RangeInputSection.tsx
@@ -1,3 +1,44 @@
-export default function RangeInputSection() {
-  return <h2>Range Input Section</h2>;
+import generateLabelName from "../../utils/generateLabelName";
+
+export type RangeInput = {
+  name: string;
+  type: string;
+  label: string;
+  defaultValue: number;
+  range: {
+    steps: number;
+    min: number;
+    max: number;
+  };
+};
+
+type RangeInputProps = {
+  input: RangeInput;
+  value: number;
+  dynamicMax?: number;
+  children?: React.ReactNode;
+  handleInputChange: (
+    e: React.ChangeEvent<HTMLInputElement>,
+    input: string
+  ) => void;
+};
+
+export default function RangeInputSection(props: RangeInputProps) {
+  const labelTag = generateLabelName(props.input.label);
+
+  return (
+    <section key={labelTag}>
+      {props.children}
+
+      <input
+        name={labelTag}
+        value={props.value}
+        type="range"
+        step={props.input.range.steps}
+        min={props.input.range.min}
+        max={props.dynamicMax ?? props.input.range.max}
+        onChange={(e) => props.handleInputChange(e, props.input.name)}
+      />
+    </section>
+  );
 }
