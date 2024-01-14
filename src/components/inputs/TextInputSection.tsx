bc60431d69c47729f732a8b6eada00159d1c32b0
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-14 20:31:20 +0000

    Update Component: Declare types, compose JSX elements + handle props

--- src/components/inputs/TextInputSection.tsx
+++ src/components/inputs/TextInputSection.tsx
@@ -1,3 +1,35 @@
-export default function TextInputSection() {
-  return <h2>Text Input Section</h2>;
+import generateLabelName from "../../utils/generateLabelName";
+
+export type TextInput = {
+  name: string;
+  type: string;
+  label: string;
+  defaultValue: number;
+};
+
+type TextInputProps = {
+  input: TextInput;
+  value: number;
+  handleInputChange: (
+    e: React.ChangeEvent<HTMLInputElement>,
+    inputName: string
+  ) => void;
+};
+
+export default function TextInputSection(props: TextInputProps) {
+  const labelTag = generateLabelName(props.input.label);
+
+  return (
+    <section key={labelTag}>
+      <section>
+        <label htmlFor={labelTag}>{props.input.label}</label>
+        <input
+          name={labelTag}
+          type="text"
+          value={props.value}
+          onChange={(e) => props.handleInputChange(e, props.input.name)}
+        />
+      </section>
+    </section>
+  );
 }
