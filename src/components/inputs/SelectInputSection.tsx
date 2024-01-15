ab25794163184f45b24b05ef702648a6dce8945f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-15 16:41:12 +0000

    Update Component: map over select options; habdle onChange via lift state callback; declare types

--- src/components/inputs/SelectInputSection.tsx
+++ src/components/inputs/SelectInputSection.tsx
@@ -1,3 +1,43 @@
-export default function SelectInputSection() {
-  return <h2>Select Input Section</h2>;
+import generateLabelName from "../../utils/generateLabelName";
+
+export type SelectInput = {
+  name: string;
+  type: string;
+  label: string;
+  defaultValue: string;
+  select: { options: string[] };
+};
+
+type SelectInputProps = {
+  input: SelectInput;
+  value?: string;
+  children?: React.ReactNode;
+  handleSelectInput: (
+    e: React.ChangeEvent<HTMLSelectElement>,
+    inputName: string
+  ) => void;
+};
+
+export default function SelectInputSection(props: SelectInputProps) {
+  const labelTag = generateLabelName(props.input.label);
+
+  return (
+    <section key={labelTag}>
+      <label htmlFor={labelTag}>{props.input.label}</label>
+      <select
+        name={labelTag}
+        onChange={(e) => props.handleSelectInput(e, props.input.name)}
+      >
+        {props.input.select.options.map((option, i) =>
+          props.input.defaultValue === option ? (
+            <option key={i} defaultValue={option}>
+              {option}
+            </option>
+          ) : (
+            <option key={i}>{option}</option>
+          )
+        )}
+      </select>
+    </section>
+  );
 }
