3d87bb0dbb216a9af244314f0c8e83c8f479fb3a
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-15 18:50:28 +0000

    Implement capital repayment calculation based on formula

--- src/utils/calculateRepayments.ts
+++ src/utils/calculateRepayments.ts
@@ -2,9 +2,38 @@ type CalcRepayParams = (
   amount: number,
   rate: number,
   years: number,
-  type: "capital" | "interest only"
+  type: "capital repayment" | "interest only"
 ) => number;
 
-const calculateRepaymenets: CalcRepayParams = (amount, rate, years, type) => {
+const calculateRepayments: CalcRepayParams = (amount, rate, years, type) => {
   if (type === "interest only") return ((amount / 100) * rate) / 12;
+
+  // capital repayment Formula [1]
+  const interestRate = rate / 12 / 100;
+  const numberOfPayments = years * 12;
+
+  const numerator = interestRate * Math.pow(1 + interestRate, numberOfPayments);
+  const denominator = Math.pow(1 + interestRate, numberOfPayments) - 1;
+
+  // capital repayment;
+  return amount * (numerator / denominator);
 };
+
+/*
+  [1] INFO: Morgage payment formula
+     LINK: https://onladder.co.uk/blog/how-to-calculate-mortgage-repayments/
+
+     P = PRINCIPAL_AMOUNT
+     N = DURATION_IN_YEARS / 12  
+
+     :: Capital ::
+     ---------------------------------------------------
+     R = (INTEREST_RATE / 100) * 12
+     I = R x (1 + (R))pow(N) ÷ (1 + R)pow(N) - 1
+     P x I
+
+     :: Interest Only ::
+     ---------------------------------------------------
+     R = INTEREST_RATE
+     ((P / 100) * R) / 12
+*/
