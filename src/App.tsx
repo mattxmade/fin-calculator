6fddb6220913bf9971f2c7e9a5e367242b52f7da
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-15 22:38:55 +0000

    Provision state logic; Implement render conditions; add localStorage support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
+import { useState } from "react";
+
 import BridgeLoanCalculator from "./components/BridgeLoanCalculator";
+import MortgageRepaymentCalculator from "./components/MortgageRepaymentCalculator";
 import "./App.css";
 
 function App() {
+  const restoreChoice = localStorage.getItem("calculator") as
+    | "Bridge"
+    | "Mortgage";
+
+  const [calculatorChoice, setcalculatorChoice] = useState<
+    "Bridge" | "Mortgage" | null
+  >(restoreChoice ?? null);
+
+  calculatorChoice
+    ? localStorage.setItem("calculator", calculatorChoice)
+    : localStorage.setItem("calculator", "");
+
   return (
-    <>
-      <BridgeLoanCalculator />
-    </>
+    <main>
+      {calculatorChoice ? (
+        <>
+          <button onClick={() => setcalculatorChoice(null)}>Back</button>
+          {calculatorChoice === "Bridge" ? (
+            <BridgeLoanCalculator />
+          ) : (
+            <MortgageRepaymentCalculator />
+          )}
+        </>
+      ) : (
+        <>
+          <h1>Choose A Calculator</h1>
+
+          <section style={{ display: "flex", gap: "1rem" }}>
+            <button onClick={() => setcalculatorChoice("Bridge")}>
+              Bridge Loan Calculator
+            </button>
+            <button onClick={() => setcalculatorChoice("Mortgage")}>
+              Mortgage Repyament Calculator
+            </button>
+          </section>
+        </>
+      )}
+    </main>
   );
 }
 
